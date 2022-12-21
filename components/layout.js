a43a6a78a993648a78f07329b5bd2856f72ac1fa
maitrakesparkbiz <nirmal.maitrak@esparkbizmail.com>
2022-12-21 10:23:59 +0530

    made error

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,6 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Maitrak";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
